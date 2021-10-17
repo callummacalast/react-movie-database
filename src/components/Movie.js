a21e2b6fb1ec5f39b2bdf22a9c2f6cf171e728ca
Callum Macalast <callummacalast@Callums-MacBook-Pro-2.local>
2021-10-17 17:26:01 +0100

    Adding React Router to the SPA

---
+++ src/components/Movie.js
@@ -0,0 +1,5 @@
+import react from "react";
+
+const Movie = () => <div>Movie</div>;
+
+export default Movie;
--- src/components/Spinner/Spinner.styles.js
+++ src/components/Spinner/Spinner.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Spinner = styled.div`
     border: 5px solid var(--lightGrey);
-    border-top: 5px solid var(--medgrey);
+    border-top: 5px solid var(--medGrey);
     border-radius: 50%;
     width: 50px;
     height: 50px;
